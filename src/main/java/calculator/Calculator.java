057e62e51cef21374bdba7cbc9ab910b22cf65f8
josefandersson <josefandersson@users.noreply.github.com>
2021-03-15 12:02:26 +0100

    upg 1 - steg 3

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,12 +8,14 @@ public class Calculator {
         System.out.println(add(""));
         System.out.println(add("1"));
         System.out.println(add("10,20"));
+        System.out.println(add("10\n20,30"));
+        System.out.println(add("1,2\n3,4"));
     }
 
     static int add(String numbers) {
         if (numbers.length() == 0)
             return 0;
-        return Arrays.stream(numbers.split(",")).mapToInt(Integer::parseInt).reduce(Integer::sum).orElse(0);
+        return Arrays.stream(numbers.split("[,\n]")).mapToInt(Integer::parseInt).reduce(Integer::sum).orElse(0);
     }
 
 }
